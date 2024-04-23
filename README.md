aa0cede232e07073866c7e87e5ad3afb380bc40c
Andreas Nielsen <andreasnielsen4000@gmail.com>
2024-04-23 15:56:11 +0200

    Made a more complete home page for playing
    Added a README.md file

---
+++ README.md
@@ -0,0 +1,32 @@
+# Azure Radio API
+
+## Concept
+
+The Azure Radio API is a web application that provides a RESTful API for accessing radio station data. We will use the API to build a radio station directory that allows users to play radio stations from urls. 
+
+## Getting Started
+
+To run the Azure Radio API locally, follow these steps:
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/<your-username>/azureradioapi.git
+    ```
+
+2. Install the dependencies:
+
+    ```bash
+    cd azureradioapi
+    npm install
+    ```
+
+4. Start the server:
+
+    ```bash
+    node app.js
+    ```
+
+5. Access the API:
+
+    Once the server is running, you can access the API at `http://localhost:3000`.
--- routes.js
+++ routes.js
@@ -5,7 +5,9 @@ const RadioBrowser = require('radio-browser');
 const urlPlaying = [
     {
         url: "",
-        name: ""
+        name: "",
+        isPlaying: true,
+        volume: 50        
     }
 ];
 
@@ -106,8 +108,29 @@ router.get('/radio/top/dk', async (request, response) => {
 // Post radio station URL
 router.post('/radio/play', async (request, response) => {
     try {
-        urlPlaying[0].url = request.body.url;
-        urlPlaying[0].name = request.body.name;
+        if (request.body.url !== undefined) {
+            urlPlaying[0].url = request.body.url;
+            if (request.body.name === undefined) {
+                let filter = {
+                    by: 'url', // stations by url
+                    searchterm: request.body.url,
+                    limit: 1
+                }
+                console.log('Filter:', filter);
+                const stations = await RadioBrowser.getStations(filter);
+                console.log('Stations:', stations);
+                urlPlaying[0].name = stations[0].name;
+            }
+        }
+        if (request.body.name !== undefined) {
+            urlPlaying[0].name = request.body.name;
+        }
+        if (request.body.isPlaying !== undefined) {
+            urlPlaying[0].isPlaying = request.body.isPlaying;
+        }
+        if (request.body.volume !== undefined) {
+            urlPlaying[0].volume = request.body.volume;
+        }
         response.send(urlPlaying);
     } catch (error) {
         console.error(error);
